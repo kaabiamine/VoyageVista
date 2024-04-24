5bea492e58b6b98855a4b963af2cfa22d8f9616b
SARRA1920 <arfaoui.sarra@esprit.tn>
2024-04-24 23:35:03 +0200

    jointure user

--- sarradb-Reservation.sql
+++ sarradb-Reservation.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Hôte : 127.0.0.1
--- Généré le : mer. 24 avr. 2024 à 19:48
--- Version du serveur : 10.4.32-MariaDB
--- Version de PHP : 8.1.25
+-- Host: 127.0.0.1
+-- Generation Time: Apr 24, 2024 at 11:34 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,48 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données : `sarradb`
+-- Database: `sarradb`
 --
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `payement`
+-- Table structure for table `doctrine_migration_versions`
+--
+
+CREATE TABLE `doctrine_migration_versions` (
+  `version` varchar(191) NOT NULL,
+  `executed_at` datetime DEFAULT NULL,
+  `execution_time` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `doctrine_migration_versions`
+--
+
+INSERT INTO `doctrine_migration_versions` (`version`, `executed_at`, `execution_time`) VALUES
+('DoctrineMigrations\\Version20240418054448', '2024-04-18 07:44:50', 88);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `messenger_messages`
+--
+
+CREATE TABLE `messenger_messages` (
+  `id` bigint(20) NOT NULL,
+  `body` longtext NOT NULL,
+  `headers` longtext NOT NULL,
+  `queue_name` varchar(190) NOT NULL,
+  `created_at` datetime NOT NULL,
+  `available_at` datetime NOT NULL,
+  `delivered_at` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `payement`
 --
 
 CREATE TABLE `payement` (
@@ -32,13 +67,24 @@ CREATE TABLE `payement` (
   `reservation_id` int(11) DEFAULT NULL,
   `mantant` double NOT NULL,
   `methode_de_payement` varchar(255) NOT NULL,
-  `rib` varchar(255) NOT NULL
+  `rib` varchar(255) NOT NULL,
+  `date_payement` date DEFAULT NULL,
+  `montant` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `payement`
+--
+
+INSERT INTO `payement` (`id`, `reservation_id`, `mantant`, `methode_de_payement`, `rib`, `date_payement`, `montant`) VALUES
+(1, 7, 1200, 'Mastercard', '12233554487', '2024-04-23', 0),
+(4, 7, 1200, 'Mastercard', '123456123456', '2024-04-24', 0),
+(5, 11, 1200, 'Visa', '123456123456', '2024-04-24', 0);
 
 -- --------------------------------------------------------
 
 --
--- Structure de la table `reservation`
+-- Table structure for table `reservation`
 --
 
 CREATE TABLE `reservation` (
@@ -50,63 +96,180 @@ CREATE TABLE `reservation` (
   `telephone` varchar(255) NOT NULL,
   `nb_enfants` int(11) NOT NULL,
   `nb_adultes` int(11) NOT NULL,
-  `status` tinyint(1) NOT NULL
+  `status` tinyint(1) NOT NULL,
+  `user_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
--- Déchargement des données de la table `reservation`
+-- Dumping data for table `reservation`
 --
 
-INSERT INTO `reservation` (`id`, `date_reservation`, `nom`, `prenom`, `email`, `telephone`, `nb_enfants`, `nb_adultes`, `status`) VALUES
-(2, '2024-04-17 00:00:00', 'eeee', 'JohnFarouk', 'john.doe@example.com', '12345678', 2, 2, 0),
-(3, '2024-04-11 09:52:00', 'Sarra', 'ch', 'farouk.chalghoumi@esprit.tn', '54303898', 2, 4, 0),
-(7, '2024-04-20 19:38:00', 'Arfaoui', 'Sarra', 'sarraarfaoui2003@gmail.com', '12345678', 1, 3, 0),
-(9, '2024-04-19 00:57:00', 'hammami', 'malek', 'hammami.malek@esprit.tn', '14785236', 0, 1, 0),
-(10, '0000-00-00 00:00:00', '', '', '', '', 0, 1, 0),
-(11, '2024-04-26 09:18:00', 'malek', 'ben said', 'arfaoui.sarra@esprit.tn', '12345698', 0, 1, 0);
+INSERT INTO `reservation` (`id`, `date_reservation`, `nom`, `prenom`, `email`, `telephone`, `nb_enfants`, `nb_adultes`, `status`, `user_id`) VALUES
+(7, '2024-04-05 15:55:00', 'Farouk Chalghoumi', 'aaaaa', 'farouk.chalghoumi031@gmail.com', '54303898', 1, 3, 1, 2),
+(8, '2024-04-05 15:55:00', 'Farouk Chalghoumi', 'aaaaa', 'farouk.chalghoumi031@gmail.com', '54303898', 1, 3, 1, 2),
+(9, '2024-04-26 13:39:00', 'Sarra', 'aaaaa', 'farouk.chalghoumi031@gmail.com', '54303898', 2, 2, 1, 2),
+(10, '2024-04-26 13:39:00', 'Farouk Chalghoumi', 'aaaaa', 'farouk.chalghoumi031@gmail.com', '54303898', 2, 2, 1, 1),
+(11, '2024-04-20 23:55:00', 'Farouk Chalghoumi', 'aaaaa', 'farouk.chalghoumi031@gmail.com', '54303898', 2, 3, 1, 1),
+(12, '2024-05-09 23:29:00', 'Farouk Chalghoumi', 'aaaaa', 'farouk.chalghoumi031@gmail.com', '54303898', 1, 3, 0, 1);
+
+-- --------------------------------------------------------
 
 --
--- Index pour les tables déchargées
+-- Table structure for table `sponsor`
+--
+
+CREATE TABLE `sponsor` (
+  `id` int(11) NOT NULL,
+  `sponsor_name` varchar(255) NOT NULL,
+  `sponsor_logo` varchar(255) NOT NULL,
+  `sponsor_description` longtext NOT NULL,
+  `sponsor_email` varchar(255) NOT NULL,
+  `sponsor_phone` varchar(255) NOT NULL,
+  `sponsor_address` varchar(255) NOT NULL,
+  `sponsor_website` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `sponsor`
+--
+
+INSERT INTO `sponsor` (`id`, `sponsor_name`, `sponsor_logo`, `sponsor_description`, `sponsor_email`, `sponsor_phone`, `sponsor_address`, `sponsor_website`) VALUES
+(1, 'hellosponsor', 'logo', 'aeaeaeaea', 'eaaa@ieee.org', '+21654303898', 'Tunis', 'website'),
+(2, '', '', '', '', '', '', ''),
+(3, '', '', '', '', '', '', ''),
+(4, '', '', '', '', '', '', ''),
+(5, '', '', '', '', '', '', ''),
+(6, '11122', '', '', '', '', '', ''),
+(7, '', '', '', '', '', '', ''),
+(8, '', '', '', '', '', '', ''),
+(9, '11', '', '', '', '', '', 'wee'),
+(10, '', '', '', '', '', '', ''),
+(11, '', '', '', '', '', '', ''),
+(12, '', '', '', '', '', '', ''),
+(13, '', '', '', '', '', '', ''),
+(14, '11', '', '', '', '', '', ''),
+(15, '', '', '', '', '', '', ''),
+(16, '', '', '', '', '', '', ''),
+(17, '', '', '', '', '', '', ''),
+(18, '', '', '', '', '', '', ''),
+(19, '', '', '', '', '', '', ''),
+(20, '', '', '', '', '', '', ''),
+(21, '', '', '', '', '', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `name`) VALUES
+(1, 'Amine'),
+(2, 'Sarra'),
+(3, 'arfaoui');
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- Index pour la table `payement`
+-- Indexes for table `doctrine_migration_versions`
+--
+ALTER TABLE `doctrine_migration_versions`
+  ADD PRIMARY KEY (`version`);
+
+--
+-- Indexes for table `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_75EA56E0FB7336F0` (`queue_name`),
+  ADD KEY `IDX_75EA56E0E3BD61CE` (`available_at`),
+  ADD KEY `IDX_75EA56E016BA31DB` (`delivered_at`);
+
+--
+-- Indexes for table `payement`
 --
 ALTER TABLE `payement`
   ADD PRIMARY KEY (`id`),
   ADD KEY `IDX_B20A7885B83297E7` (`reservation_id`);
 
 --
--- Index pour la table `reservation`
+-- Indexes for table `reservation`
 --
 ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `IDX_42C84955A76ED395` (`user_id`);
+
+--
+-- Indexes for table `sponsor`
+--
+ALTER TABLE `sponsor`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT pour les tables déchargées
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT pour la table `payement`
+-- AUTO_INCREMENT for table `messenger_messages`
+--
+ALTER TABLE `messenger_messages`
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `payement`
 --
 ALTER TABLE `payement`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- AUTO_INCREMENT pour la table `reservation`
+-- AUTO_INCREMENT for table `reservation`
 --
 ALTER TABLE `reservation`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
--- Contraintes pour les tables déchargées
+-- AUTO_INCREMENT for table `sponsor`
+--
+ALTER TABLE `sponsor`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
 --
 
 --
--- Contraintes pour la table `payement`
+-- Constraints for table `payement`
 --
 ALTER TABLE `payement`
   ADD CONSTRAINT `FK_B20A7885B83297E7` FOREIGN KEY (`reservation_id`) REFERENCES `reservation` (`id`);
+
+--
+-- Constraints for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD CONSTRAINT `FK_42C84955A76ED395` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
