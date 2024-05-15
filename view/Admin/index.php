4943a9021eda287cd8ee97e6193bd98fb404c492
SARRA1920 <arfaoui.sarra@esprit.tn>
2024-05-15 03:18:13 +0200

    Reservation Back-Office Merge

--- view/Admin/index.php
+++ view/Admin/index.php
@@ -255,12 +255,12 @@ $user = $user->getUser($user1['id']);
           <li class="nav-item">
             <a class="nav-link" data-toggle="collapse" href="#charts" aria-expanded="false" aria-controls="charts">
               <i class="typcn typcn-chart-pie-outline menu-icon"></i>
-              <span class="menu-title">Charts</span>
+              <span class="menu-title">Gestion Reservation & payement</span>
               <i class="menu-arrow"></i>
             </a>
             <div class="collapse" id="charts">
               <ul class="nav flex-column sub-menu">
-                <li class="nav-item"> <a class="nav-link" href="pages/charts/chartjs.html">ChartJs</a></li>
+                <li class="nav-item"> <a class="nav-link" href="./AfficherReservations.php">Reservations</a></li>
               </ul>
             </div>
           </li>
